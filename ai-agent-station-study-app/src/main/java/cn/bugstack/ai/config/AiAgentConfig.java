be26d3935770f327b66d35e426c69d539b89857a
zhangyj <2786466348@qq.com>
2026-08-26 23:01:30 +0800

    refactor(config): 优化AiAgentConfig配置类

--- ai-agent-station-study-app/src/main/java/cn/bugstack/ai/config/AiAgentConfig.java
+++ ai-agent-station-study-app/src/main/java/cn/bugstack/ai/config/AiAgentConfig.java
@@ -6,7 +6,6 @@ import org.springframework.ai.transformer.splitter.TokenTextSplitter;
 import org.springframework.ai.vectorstore.pgvector.PgVectorStore;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -14,22 +13,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 @Configuration
 public class AiAgentConfig {
 
-    /**
-     * -- 删除旧的表（如果存在）
-     * DROP TABLE IF EXISTS public.vector_store_openai;
-     * <p>
-     * -- 创建新的表，使用UUID作为主键
-     * CREATE TABLE public.vector_store_openai (
-     * id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-     * content TEXT NOT NULL,
-     * metadata JSONB,
-     * embedding VECTOR(1536)
-     * );
-     * <p>
-     * SELECT * FROM vector_store_openai
-     */
     @Bean("vectorStore")
-    @ConditionalOnBean(name = "pgVectorJdbcTemplate")
     public PgVectorStore pgVectorStore(@Value("${spring.ai.openai.embedding.base-url:https://api.openai.com/v1}") String embeddingBaseUrl,
                                        @Value("${spring.ai.openai.embedding.api-key:${spring.ai.openai.api-key}}") String embeddingApiKey,
                                        @Qualifier("pgVectorJdbcTemplate") JdbcTemplate jdbcTemplate) {
